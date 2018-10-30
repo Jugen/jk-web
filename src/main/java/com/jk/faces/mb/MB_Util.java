5cc6ddeea4ae7b47187bf946eb0cb4f5f025d557
Jalal Kiswani <kiswanij@gmail.com>
2018-10-30 14:45:25 -0700

    .

--- src/main/java/com/jk/faces/mb/MB_Util.java
+++ src/main/java/com/jk/faces/mb/MB_Util.java
@@ -4,6 +4,7 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
 import com.jk.util.JK;
+import com.jk.util.JKDateTimeUtil;
 
 @ManagedBean(name="util")
 @ApplicationScoped
@@ -12,5 +13,9 @@ public class MB_Util {
 	public int reloadRandom() {
 		//TODO: make this return same value in production envi.
 		return JK.randomNumber();
+	}
+	
+	public int currentYear() {
+		return JKDateTimeUtil.getCurrentYear();
 	}
 }
