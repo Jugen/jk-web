c60db489d035776e71b3f2c826d4fe8ec7d1d111
Jalal Kiswani <kiswanij@gmail.com>
2018-11-14 10:51:23 -0800

    .

--- src/main/java/com/jk/faces/tags/JKTagIncludeHandler.java
+++ src/main/java/com/jk/faces/tags/JKTagIncludeHandler.java
@@ -2,11 +2,15 @@ package com.jk.faces.tags;
 
 import com.jk.faces.util.JKJsfUtil;
 import com.jk.util.JKIOUtil;
+import com.jk.util.logging.JKLogger;
+import com.jk.util.logging.JKLoggerFactory;
 import com.sun.faces.facelets.el.VariableMapperWrapper;
+import com.sun.faces.taglib.html_basic.PanelGroupTag;
 import com.sun.faces.util.FacesLogger;
 
 import javax.el.VariableMapper;
 import javax.faces.component.UIComponent;
+import javax.faces.view.facelets.ComponentHandler;
 import javax.faces.view.facelets.FaceletContext;
 import javax.faces.view.facelets.TagAttribute;
 import javax.faces.view.facelets.TagAttributeException;
@@ -19,9 +23,11 @@ import java.util.logging.Logger;
 import javax.faces.view.facelets.TagException;
 import javax.faces.view.facelets.TagHandler;
 
+import org.primefaces.component.panel.Panel;
+
 public final class JKTagIncludeHandler extends TagHandler {
 
-	private static final Logger log = FacesLogger.FACELETS_INCLUDE.getLogger();
+	private final JKLogger logger = JKLoggerFactory.getLogger(getClass());
 
 	private final TagAttribute value;
 
@@ -45,23 +51,24 @@ public final class JKTagIncludeHandler extends TagHandler {
 	 * javax.faces.component.UIComponent)
 	 */
 	public void apply(FaceletContext ctx, UIComponent parent) throws IOException {
-		String value =  this.value.getValue(ctx);//(String) JKJsfUtil.evaluateExpressionToObject(this.value.getValue(ctx));
-		if (!value.equals("")) {
-			File file = JKIOUtil.writeDataToTempFile(value, ".xhtml");
+		VariableMapper orig = ctx.getVariableMapper();
+		ctx.setVariableMapper(new VariableMapperWrapper(orig));
 
-			VariableMapper orig = ctx.getVariableMapper();
+		logger.trace("JK include tag ...");
+		String value = this.value.getValue(ctx);// (String) JKJsfUtil.evaluateExpressionToObject(this.value.getValue(ctx));
+		if (!value.equals("")) {
+			File file = JKIOUtil.writeDataToTempFile(value, "xhtml");
+			logger.debug("Including value file ({}))", file.getAbsolutePath());
 			ctx.setVariableMapper(new VariableMapperWrapper(orig));
 			try {
 				this.nextHandler.apply(ctx, null);
-				ctx.includeFacelet(parent, file.toURL());
-			} catch (IOException e) {
-				if (log.isLoggable(Level.FINE)) {
-					log.log(Level.FINE, e.toString(), e);
-				}
-				throw new TagAttributeException(this.tag, this.value);
+				ctx.includeFacelet(parent, file.toURI().toURL());
+			} catch (Exception e) {
+				logger.error("Failed to include file ({})", JKIOUtil.readFile(file));
+				throw new TagAttributeException(this.tag, this.value, e);
 			} finally {
 				ctx.setVariableMapper(orig);
-				file.delete();
+				file.deleteOnExit();
 			}
 		}
 	}
